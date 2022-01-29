1aa3dacb8adbd8789c55e7e2c7634e5dcee6bf0e
JangAyeon <ayeon3052@ewhain.net>
2022-01-29 18:46:07 +0900

    #3 form address

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -21,8 +21,9 @@ application/json
 */
 
 import React, { useState } from "react";
+import Post from "../../Components/Post/Post";
 
-function Signup() {
+const Signup = () => {
   const [inputs, setInputs] = useState({
     Id: "",
     showIdInfo: false,
@@ -30,9 +31,12 @@ function Signup() {
     showPwdInfo: false,
     RePwd: "",
     showRePwdInfo: false,
-    username: "",
-    phone: "",
-    email: "",
+    Username: "",
+    Phone: "",
+    Email: "",
+    showAddressPop: false,
+    Address: "",
+    Zonecode: "",
   });
 
   const {
@@ -42,23 +46,37 @@ function Signup() {
     showPwdInfo,
     RePwd,
     showRePwdInfo,
-    username,
-    phone,
-    email,
+    Username,
+    Phone,
+    Email,
+    showAddressPop,
+    Address,
+    Zonecode,
   } = inputs;
 
   const onChange = (e) => {
     const { value, name } = e.target;
+    console.log(e.target);
     setInputs({
       ...inputs,
       [name]: value,
       //핸드폰번호 text아닌 숫자로 입력될까봐...
     });
+    console.log(inputs);
   };
 
-  const onClick = (e) => {
-    console.log(e.target.name);
+  const onClick_Info = (e) => {
+    console.log(e.target);
     let name = `show${e.target.name}Info`;
+    setInputs({
+      ...inputs,
+      [name]: true,
+    });
+  };
+
+  const onClick_Pop = (e) => {
+    console.log(e.target.name);
+    let name = `show${e.target.name}Pop`;
     setInputs({
       ...inputs,
       [name]: true,
@@ -80,67 +98,73 @@ function Signup() {
           <div className="signup_Id_title">
             아이디<span className="ico">*</span>
           </div>
-          <input
-            className="signup_Id_input"
-            type="text"
-            name="Id"
-            placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
-            maxLength="16"
-            onChange={onChange}
-            value={Id}
-            onClick={onClick}
-          />
+          <div className="signup_Id_block">
+            <input
+              className="signup_Id_input"
+              type="text"
+              name="Id"
+              placeholder="6자 이상의 영문 혹은 영문과 숫자를 조합"
+              maxLength="16"
+              onChange={onChange}
+              value={Id}
+              onClick={onClick_Info}
+            />
+            {showIdInfo ? (
+              <div className="signup_Id_info">
+                <li>6자 이상의 영문과 숫자를 조합</li>
+                <li> 아이디 중복 확인</li>
+              </div>
+            ) : null}
+          </div>
           <div className="signup_Id_check">중복확인</div>
         </div>
-        {showIdInfo ? (
-          <div className="signup_Id_info">
-            <li>6자 이상의 영문과 숫자를 조합</li>
-            <li> 아이디 중복 확인</li>
-          </div>
-        ) : null}
 
         <div className="signup_Pw_wrap">
           <div className="signup_Pw_title">
             비밀번호<span className="ico">*</span>
           </div>
-          <input
-            className="signup_Pw_input"
-            name="Pwd"
-            placeholder="비밀번호를 입력해주세요"
-            maxLength="16"
-            onChange={onChange}
-            value={Pwd}
-            onClick={onClick}
-          />
-          {showPwdInfo ? (
-            <div className="signup_Id_info">
-              <li>10자 이상 입력</li>
-              <li> 영문/숫자/특수문자(공백 제외)만 허용하며, 2개 이상 조합</li>
-              <li>동일한 숫자 3개 이상 연속 사용 불가</li>
-            </div>
-          ) : null}
+          <div className="signup_Pw_block">
+            <input
+              className="signup_Pw_input"
+              name="Pwd"
+              placeholder="비밀번호를 입력해주세요"
+              maxLength="16"
+              onChange={onChange}
+              value={Pwd}
+              onClick={onClick_Info}
+            />
+            {showPwdInfo ? (
+              <div className="signup_Id_info">
+                <li>10자 이상 입력</li>
+                <li>영문/숫자/특수문자(공백 제외)만 허용하며, 2개 이상 조합</li>
+                <li>동일한 숫자 3개 이상 연속 사용 불가</li>
+              </div>
+            ) : null}
+          </div>
         </div>
 
         <div className="signup_RePw_wrap">
           <div className="signup_RePw_title">
-            비밀번호<span className="ico">*</span>
+            비밀번호 확인<span className="ico">*</span>
           </div>
-          <input
-            className="signup_RePw_input"
-            type="text"
-            name="RePwd"
-            placeholder="비밀번호를 한번 더 입력해주세요"
-            maxLength="16"
-            onChange={onChange}
-            value={RePwd}
-            onClick={onClick}
-          />
+          <div className="signup_RePw_block">
+            <input
+              className="signup_RePw_input"
+              type="text"
+              name="RePwd"
+              placeholder="비밀번호를 한번 더 입력해주세요"
+              maxLength="16"
+              onChange={onChange}
+              value={RePwd}
+              onClick={onClick_Info}
+            />
+            {showRePwdInfo ? (
+              <div className="signup_Id_info">
+                <li>동일한 비밀번호를 입력해주세요</li>
+              </div>
+            ) : null}
+          </div>
         </div>
-        {showRePwdInfo ? (
-          <div className="signup_Id_info">
-            <li>동일한 비밀번호를 입력해주세요</li>
-          </div>
-        ) : null}
 
         <div className="signup_Username_wrap">
           <div className="signup_Username_title">
@@ -149,12 +173,13 @@ function Signup() {
           <input
             className="signup_Username_input"
             type="text"
-            name="username"
+            name="Username"
             placeholder="이름을 입력해주세요"
             onChange={onChange}
-            value={username}
+            value={Username}
           />
         </div>
+
         <div className="signup_Email_wrap">
           <div className="signup_Email_title">
             이메일<span className="ico">*</span>
@@ -162,13 +187,14 @@ function Signup() {
           <input
             className="signup_Email_input"
             type="text"
-            name="email"
+            name="Email"
             placeholder="예:markerkeyrly@kurly.com"
             onChange={onChange}
-            value={email}
+            value={Email}
           />
           <div className="signup_Email_check">중복확인</div>
         </div>
+
         <div className="signup_Phone_wrap">
           <div className="signup_Phone_title">
             휴대폰<span className="ico">*</span>
@@ -176,17 +202,80 @@ function Signup() {
           <input
             className="signup_Phone_input"
             type="text"
-            name="phone"
+            name="Phone"
             placeholder="숫자 입력해주세요"
             pattern="[0-9]*"
             onChange={onChange}
-            value={phone}
+            value={Phone}
           />
           <div className="signup_Phone_check">중복확인</div>
+        </div>
+
+        <div className="signup_Address_wrap">
+          <div className="signup_Address_title">
+            주소 <span className="ico">*</span>
+          </div>
+          <div className="signup_Address_block">
+            {showAddressPop ? (
+              <Post
+                getData={(fullAddress, zoneCode) => {
+                  console.log(fullAddress, zoneCode);
+                  setInputs({
+                    ...inputs,
+                    Address: fullAddress,
+                    Zonecode: zoneCode,
+                    showAddressPop: false,
+                    //핸드폰번호 text아닌 숫자로 입력될까봐...
+                  });
+                }}
+              />
+            ) : null}
+            {Zonecode ? (
+              <input type="text" defaultValue={Zonecode}></input>
+            ) : null}
+            {Address ? (
+              <input type="text" defaultValue={Address}></input>
+            ) : null}
+
+            {!(Zonecode && Address) ? (
+              <button name="Address" onClick={onClick_Pop}>
+                🔍︎ 주소 검색
+              </button>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  placeholder="나머지 주소를 입력하시오"
+                  onChange={(e) => {
+                    setInputs({
+                      ...inputs,
+                      Address: `${Address} ${String(e.target.value)}`,
+                    });
+                    console.log(inputs);
+                  }}
+                ></input>
+                <button name="Address" onClick={onClick_Pop}>
+                  🔍︎ 주소 재검색
+                </button>
+                <p>샛별 배송 가능 지역</p>
+              </>
+            )}
+
+            <div className="signup_Address_Info">
+              <p>배송지에 따라 상품 정보가 달라질 수 있습니다.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="signup_Sex_wrap">
+          <div className="signup_Sex_title">
+            성별<span className="ico">*</span>
+          </div>
+          <div className="signup_Sex_block">성별칸!!</div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Signup;
