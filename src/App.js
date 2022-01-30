bf28d7657db09700389c4c0215f61a2a665a460f
JangAyeon <ayeon3052@ewhain.net>
2022-01-30 23:57:09 +0900

    login & member join HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
-
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
 function App() {
   return (
     <>
+      <Login />
+      <Signup />
       <Footer />
     </>
   );
