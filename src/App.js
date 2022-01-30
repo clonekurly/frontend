01d6ed815d88507bdd16ede5a33caf86d6d1c322
JangAyeon <ayeon3052@ewhain.net>
2022-01-30 23:52:28 +0900

    PR

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
-import Signup from "./Pages/Signup/Signup";
+
 function App() {
   return (
     <>
-      <Signup />
       <Footer />
     </>
   );
