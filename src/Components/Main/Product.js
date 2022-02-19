c6512f2ad2094a0eb62faead31803be0c0d5e232
heesoo <11junghs@gmail.com>
2022-02-19 20:44:30 +0900

    [2월 19일] 상품 axios 추가

--- src/Components/Main/Product.js
+++ src/Components/Main/Product.js
@@ -5,11 +5,11 @@ const product = (props) => {
     // const {id, img_data, name, price, info} =props;
     return (
         <div className='prod_padding'>
-            <img className="prod_img" margin = "10px"width="250px" height="315px" src={ props.data.img_data} width="100%" />
+            <img className="prod_img" margin = "10px"width="250px" height="315px" src={ props.data.img_url} width="100%" />
             <br/><br/>
             <h3 className='text1'>{props.data.name}</h3>
             <br/>
-            <h3 className='text2'>{props.data.price}원</h3>
+            <h3 className='text2'>{props.data.original_price}원</h3>
          
         </div>
     );
--- src/Pages/Main_sampleData.js
+++ src/Pages/Main_sampleData.js
@@ -2,37 +2,37 @@ export default
 [
     {
       id:1,
-      img_data: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
+      img_url: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
       name: '[풀무원] 올바른 핫도그 5개입',
-      price: 5985,
+      original_price: 5985,
       info: ''
     },
     {
       id:1,
-      img_data: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
+      img_url: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
       name: '[스테파노스 키친]뽀모도로 스파게티',
-      price: 4092,
+      original_price: 4092,
       info: ''
     },
     {
       id:1,
-      img_data: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
+      img_url: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
       name: '[발더라마]오칼(Ocal)',
-      price: 20900,
+      original_price: 20900,
       info: ''
     },
     {
       id:1,
-      img_data: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
+      img_url: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
       name: '[청오건강] 유기농 쌀가루',
-      price: 3350,
+      original_price: 3350,
       info: ''
     },
     {
       id:1,
-      img_data: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
+      img_url: "https://i.pinimg.com/564x/bf/35/66/bf3566cd10c73edcfb61e041dbb434a1.jpg",
       name: '[올리타리아] 포도씨유',
-      price: 7770,
+      original_price: 7770,
       info: ''
     },
   ];
