7d245189cd77f9fe32e6746cddfd002c8e6e568a
heesoo <11junghs@gmail.com>
2022-02-19 20:50:36 +0900

    Revert "Merge pull request #9 from clonekurly/origin/feature/mainPage"
    This reverts commit 37208007915e305162891d3c8b8523267cc13f46, reversing
    changes made to d6fff9bdbb25943012b8175ccaebe98d2c7c214c.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Main from './Pages/Main';
-import Footer from './Components/Footer/Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      
-      <Main />
+    <>
+      {/*<Signup />*/}
       <Footer />
-    </div>
+    </>
   );
 }
 
